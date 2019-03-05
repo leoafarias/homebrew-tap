9f5dc53c226ebe476e50d458b5a50009d40d919a
goreleaserbot <goreleaser@carlosbecker.com>
2019-03-05 15:05:55 -0500

    Brew formula update for fvm version v0.2.8

--- Formula/fvm.rb
+++ Formula/fvm.rb
@@ -2,9 +2,9 @@
 class Fvm < Formula
   desc "Flutter version management tool"
   homepage "https://github.com/leoafarias/fvm"
-  url "https://github.com/leoafarias/fvm/releases/download/v0.2.7/fvm_0.2.7_Darwin_x86_64.tar.gz"
-  version "0.2.7"
-  sha256 "87ca231afd30ff7242f561ecd7ac9729ffa599139d40fbc0ffcf7993bed15c2f"
+  url "https://github.com/leoafarias/fvm/releases/download/v0.2.8/fvm_0.2.8_Darwin_x86_64.tar.gz"
+  version "0.2.8"
+  sha256 "3e0a4f1f24b7f61bc7ed9c08e3b0e945bcd56af406c86a64710de136a78675bc"
   
   depends_on "git"
 
